1508da5b37d8f1a8774caaa1e369001e2a330904
Davidov249 <davidov249@gmail.com>
2018-11-24 00:47:32 -0600

    vistas chat

--- app/src/main/java/com/example/sthephan/proyectoedii/Chat.java
+++ app/src/main/java/com/example/sthephan/proyectoedii/Chat.java
@@ -2,12 +2,14 @@ package com.example.sthephan.proyectoedii;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Chat extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
     }
 }
